c06d4a100124b1e005c0f07eb816536168491e4a
Sambid10 <sambidshk10@gmail.com>
2025-01-23 17:58:24 +0545

    changes to reac-icns

--- src/lib/rag-chat.ts
+++
@@ -1,7 +0,0 @@
-import {RAGChat, upstash} from "@upstash/rag-chat"
-import { redis } from "./redis"
-
-export const ragchat=new RAGChat({
-    model:upstash("mistralai/Mistral-7B-Instruct-v0.2"),
-    redis:redis
-})
