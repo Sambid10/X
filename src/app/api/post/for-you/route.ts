28f4da6305257e4762ee67be85bfbaf644d543be
Sambid10 <sambidshk10@gmail.com>
2025-01-24 16:10:54 +0545

    for-you page problem still

--- src/app/api/post/for-you/route.ts
+++ src/app/api/post/for-you/route.ts
@@ -2,28 +2,29 @@ import { db } from "@/lib/db"
 import getSession from "@/lib/getSession"
 import { getPostDataInclude, PostPage } from "@/lib/types"
 import { NextRequest } from "next/server"
-
-export async function GET(req:NextRequest) {
+export async function GET(req:NextRequest){
     try{
-        const cursor=req.nextUrl.searchParams.get("cursor") || undefined
-        const pageSize=5
+
         const session=await getSession()
-        if(!session?.user.id) return Response.json({error:"Unauthirized"},{status:401})
-        const post=await db.post.findMany({
-            include:getPostDataInclude(session.user.id),
-            orderBy:{createdAt:"desc"},
-            take:pageSize+1,
-            cursor:cursor ? {id:cursor} : undefined
+        if(!session?.user.id)  return Response.json({error:"Unauthorzied"},{status:401})
+        const pageSize=10
+        const cursor=req.nextUrl.searchParams.get("cursor") || undefined
+        const followingPost=await db.post.findMany({
+           include:getPostDataInclude(session.user.id),
+           take:pageSize+1,
+           cursor:cursor ? {id:cursor} : undefined,
+           orderBy:{
+            createdAt:"desc"
+           }
         })
-       
-        const nextCursor=post.length > pageSize ? post[pageSize].id : null
+        const nextcusror=followingPost.length > pageSize ? followingPost[pageSize].id : null
         const data:PostPage={
-            posts:post.slice(0,pageSize),
-            nextCursor:nextCursor
+            nextCursor:nextcusror,
+            posts:followingPost.slice(0,pageSize)
         }
         return Response.json(data)
-        }catch(error){
-        console.log(error)
-        return Response.json({error:"Internal Server error"},{status:500})
+    }catch(err){
+        console.log(err)
+        return Response.json({error:"Server Error"},{status:501})
     }
 }
